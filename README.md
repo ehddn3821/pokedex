e637ed779fee14f42d24eb66c3afe20f322bbedf
dwkang <ehddn3821@naver.com>
2021-06-29 22:04:22 +0900

    Feat: Remove PokeAPI and replace with WebScraper

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-# pokedex
-=======
-<<<<<<< HEAD
-# pokedex
-=======
-# pokedex
+# Pokedex
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
->>>>>>> 7372562 (Feat: Bottom tab navigation, Pokemon API connection)
->>>>>>> ef0cd48 (Feat: Bottom tab navigation, Pokemon API connection)
+포켓몬도감 번호
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,38 +1,44 @@
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:pokedex/models/poke.dart';
-import 'package:http/http.dart' as http;
+import 'package:web_scraper/web_scraper.dart';
 
-Future<Poke> fetchPoke() async {
-  final response = await http.get(
-      Uri.parse('https://pokeapi.co/api/v2/pokemon-species/12'));
-
-  if (response.statusCode == 200) {
-    return Poke.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Failed to load pokemon');
-  }
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
-class HomePage extends StatelessWidget {
+class _HomePageState extends State<HomePage> {
+
+  var nameList = <String>[];
+
+  @override
+  void initState() {
+    super.initState();
+    initPokeScrap();
+  }
 
   @override
   Widget build(BuildContext context) {
 
-    return Center(
-      child: FutureBuilder<Poke>(
-        future: fetchPoke(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Text(snapshot.data!.names[2].name);
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          }
-          return CircularProgressIndicator();
-        },
-      ),
-    );
+    return Center(child: Text('$nameList'));
+  }
+
+  void initPokeScrap() async {
+    final webScraper = WebScraper('https://www.pokemonkorea.co.kr');
+    final endpoint = '/pokedex?word=2';
+    if (await webScraper.loadWebPage(endpoint)) {
+      final nameElements = webScraper.getElement(
+          '#pokedexlist > li > a > div.bx-txt > h3',
+          []
+      );
+      final nameList = <String>[];
+      nameElements.forEach((element) {
+        final name = element['title'];
+        nameList.add('$name');
+      });
+      if (mounted)
+        setState(() {
+          this.nameList = nameList;
+        });
+    }
   }
 }
